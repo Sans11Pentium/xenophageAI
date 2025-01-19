455507220831b59ea9b367173868fda8c4cb28c8
Sans11Pentium <sanskriti.ug22@nsut.ac.in>
2025-01-19 12:30:17 +0530

    eslint config added

--- src/lib/database/prisma/prismaClient.ts
+++ src/lib/database/prisma/prismaClient.ts
@@ -1,11 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma_: PrismaClient | undefined; // Declare prisma on the global object
+  // Extend the global type to include the `prisma_` property
+  namespace NodeJS {
+    interface Global {
+      prisma_: PrismaClient | undefined;
+    }
+  }
+
+  // Add to globalThis for proper TS recognition
+  var prisma_: PrismaClient | undefined;
 }
 
-const prisma=globalThis.prisma_ || new PrismaClient();
+const prisma = globalThis.prisma_ || new PrismaClient();
 
-if(process.env.NODE_ENV !== 'production') globalThis.prisma_ = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma_ = prisma;
+}
 
 export default prisma;
